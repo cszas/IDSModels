e3880ee8363baea8f6a46163b0bbf0b3b2dc77f0
Carmen <Carmen@MacBook-Pro-de-Carmen-2.local>
2020-06-14 21:37:28 +0200

    Add balanced models

---
+++ balanced/data_stream.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from kafka 						import KafkaProducer
+import time
+
+# Open features
+file = open("./data/x_test_corr.csv","r") #Solo lectura
+
+# Send to Kafka Broker
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+
+for line in file:
+	if not line.__contains__('dur'):
+		print(line)
+		producer.send('Streaming_TFM',line.encode('utf-8')) #Topic: Streaming_TFM
+		time.sleep(1)
+
+
